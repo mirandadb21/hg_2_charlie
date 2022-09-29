aad4ebb790e895c0372bf6682ef898f7fa035fe2
adrianbetancourt141@gmail.com <adrianbetancourt141@gmail.com>
2022-09-28 23:08:14 -0400

    feat: send folder with the 10 hacks

---
+++ charlie/charlie_hack4.js
@@ -0,0 +1,4 @@
+let listaa = [100,200,300,400,500,600,700]
+
+listaa.splice(2,1)
+listaa.splice(3,1)
---
+++ charlie/charlie_hacks7.js
@@ -0,0 +1,3 @@
+let listaa = [100,200,300,400,500,600,700]
+listaa.splice(2,1,"charlie");
+console.log(listaa);
